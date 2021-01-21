a52a69b04f6eb067c68053e344567ca98fe9e62c
KevinG99 <56520186+KevinG99@users.noreply.github.com>
2021-01-21 14:07:38 +0100

    uebungen

--- app/src/main/java/com/example/appexample/Notifications.kt
+++ app/src/main/java/com/example/appexample/Notifications.kt
@@ -17,10 +17,10 @@ fun NotificationManager.sendshortNotification(
     messagebody: String, title: String, context: Context
 ) {
     val contentIntent =
-        Intent(context, MainActivity2::class.java)             //create Intent, to MainActivity2
+        Intent(context, MainActivity2::class.java)
 
     val contentPendingIntent =
-        PendingIntent.getActivity(                             //erzeugt Pending Intent
+        PendingIntent.getActivity(
             context,
             NOTIFICATION_ID_0,
             contentIntent,
@@ -31,15 +31,15 @@ fun NotificationManager.sendshortNotification(
         context,
         context.getString(R.string.channelID)
     )
-        .setSmallIcon(R.drawable.ic_short_notification)        //muss gesetzt sein
-        .setContentTitle(title)                                //muss gesetzt sein
-        .setContentText(messagebody)                           //muss gesetzt sein
+        .setSmallIcon(R.drawable.ic_short_notification)
+        .setContentTitle(title)
+        .setContentText(messagebody)
         .setPriority(NotificationCompat.PRIORITY_HIGH)
-        .setContentIntent(contentPendingIntent)                //ueberlaesst der fremden application mein gesetztes Intent --> Main Activity
-        .setAutoCancel(true)                                   //entfernt die Notification, beim Klick
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
 
 
-    notify(                                                    //automatisches updaten der letzten Benachrichtigung
+    notify(
         NOTIFICATION_ID_0,
         builder.build()
     )
@@ -54,63 +54,27 @@ fun NotificationManager.sendexpandableNotification(
     val rgb = ContextCompat.getColor(context, R.color.purple_500)
 
     //TODO: Step 1.1 create Intent
-    val contentIntent =
-        Intent(context, MainActivity::class.java)    //Intent erzeugt, zur MainActivity
 
     //TODO: Step 1.2 create PendingIntent
-    val contentPendingIntent =
-        PendingIntent.getActivity(                   //erzeugt Pending Intent
-            context,
-            NOTIFICATION_ID_1,
-            contentIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT
-        )
 
     //TODO: Step 1.3 add sendNew Action (GIVEN)
-    val alarmIntent = Intent(context, AlarmReceiver::class.java)
-    val pendingIntent = PendingIntent.getBroadcast(
-        context,
-        NOTIFICATION_ID_1,
-        alarmIntent,
-        PendingIntent.FLAG_UPDATE_CURRENT
-    )
-
 
     //TODO: Step 1.4 get an instance of NotificationCompat.Builder
-    val builder = NotificationCompat.Builder(
-        context,
-        context.getString(R.string.secChannelID)
-    )
+
         //TODO: Step 1.5 set title, text and icon to builder
-        .setContentTitle(title)
-        .setContentText(messagebody)
-        .setSmallIcon(R.drawable.ic_expandable_notification)
 
         //TODO: Step 1.6 set priority
-        .setPriority(NotificationCompat.PRIORITY_HIGH)
 
         //TODO: Step 1.7 set content Intent
-        .setContentIntent(contentPendingIntent)
-        .setAutoCancel(true)
 
         //TODO: Step 1.8 add color style
-        .setColorized(true)                          //enable background color if the activity is in a foreground
-        .setColor(rgb)                               //setze Farbe fuer Icon und Buttons
 
         //TODO: Step 1.9 add style to builder
-        .setStyle(
-            NotificationCompat.BigTextStyle()        //laesst den Text im Screen erscheinen und zeigt einen
-        )                                            //groesseren message Body an
 
         //TODO: Step 1.10 add Action to send a new Notification
-        .addAction(
-            R.drawable.ic_expandable_notification_32,
-            "send new",
-            pendingIntent
-        )
+
 
     //TODO: Step 1.11 call notify
-    notify(NOTIFICATION_ID_1, builder.build())
 }
 
 
@@ -126,12 +90,9 @@ fun createNotificationChannel(
         channelName,
         NotificationManager.IMPORTANCE_DEFAULT
     ).apply {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {       //check if the Build SDK is bigger than 29, to use canBubble()
             description = channelDescription                //set channel description
             enableVibration(true)                 //enable vibration
             enableLights(true)                      //enable the notification LED
-            canBubble()                                    //lets the notification peeks into the Screen
-           }
     }
 }
 
